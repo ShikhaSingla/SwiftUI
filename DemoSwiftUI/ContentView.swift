370a69c6b65b95927efb3d3e0c34057659d2623c
ShikhaSingla <shikha.singla@trantorinc.com>
2020-01-24 14:06:10 +0530

    Initial Commit

---
+++ DemoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoSwiftUI
+//
+//  Created by IOS on 24/01/20.
+//  Copyright © 2020 IOS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
