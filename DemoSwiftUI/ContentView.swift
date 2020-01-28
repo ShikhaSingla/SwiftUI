abda33ed1fff9f7e5007cfe3d88ad8ee8cadb250
ShikhaSingla <shikha.singla@trantorinc.com>
2020-01-28 19:44:29 +0530

    demo swiftUI tableview

--- DemoSwiftUI/ContentView.swift
+++ DemoSwiftUI/ContentView.swift
@@ -9,13 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var tutors: [Tutor] = []
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(tutors) { tutor in
+                TutorCell(tutor: tutor)
+            }
+            .navigationBarTitle("Tutors")
+        }
+        
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(tutors: testData)
+    }
+}
+#endif
+
+struct TutorCell: View {
+    let tutor: Tutor
+    
+    var body: some View {
+        NavigationLink(destination: Text(tutor.name)) {
+            Image(tutor.imageName)
+                .cornerRadius(40.0)
+            VStack(alignment: .leading) {
+                Text(tutor.name)
+                Text(tutor.headline)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
     }
 }
