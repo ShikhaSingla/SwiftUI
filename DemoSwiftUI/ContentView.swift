3bb94d9b188562db10b195c1735e6ec135c84052
ShikhaSingla <shikha.singla@trantorinc.com>
2020-01-28 19:57:16 +0530

    --

--- DemoSwiftUI/ContentView.swift
+++ DemoSwiftUI/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
             List(tutors) { tutor in
                 TutorCell(tutor: tutor)
             }
+                
             .navigationBarTitle("Tutors")
         }
         
